7b78b4d698bdaa60fd2128b5fd6541a374a7c24d
Ryo <muhammadryo15@gmail.com>
2026-02-14 10:57:04 +0800

    First commit

---
+++ app/src/main/java/com/example/navi/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.navi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val logo = findViewById<ImageView>(R.id.IMGLogo)
+
+        // animasi fade in
+        logo.alpha = 0f
+        logo.animate()
+            .alpha(1f)
+            .setDuration(1000)
+            .withEndAction {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this, LoginForm::class.java))
+                    overridePendingTransition(
+                        android.R.anim.fade_in,
+                        android.R.anim.fade_out
+                    )
+                    finish()
+
+                }, 1500) // delay 0.5 detik
+
+
+            }
+    }
+}
