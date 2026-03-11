f67eefdebc384242e08fb60d5be5be3accc6bd7b
Ryo <muhammadryo15@gmail.com>
2026-03-11 11:15:21 +0800

    ui register

--- app/src/main/java/com/example/navi/RegisterForm.kt
+++ app/src/main/java/com/example/navi/RegisterForm.kt
@@ -1,20 +1,39 @@
 package com.example.navi
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.navi.data.AppDatabase
+import com.example.navi.data.User
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.launch
 
 class RegisterForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register_form)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etUsername = findViewById<TextInputEditText>(R.id.Username)
+        val etEmail  = findViewById<TextInputEditText>(R.id.tvEmail)
+        val etPassword = findViewById<TextInputEditText>(R.id.Password)
+        val db = AppDatabase.getDatabase(this)
+        lifecycleScope.launch {
+
+            val user = User(
+                username = etUsername.text.toString(),
+                email = etEmail.text.toString(),
+                password = etPassword.text.toString()
+            )
+
+            db.userDao().insertUser(user)
+
+            Toast.makeText(this@RegisterForm, "Register berhasil", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
