d6947b422d5933414d37e9e5778fdc9cc493b463
Ryo <muhammadryo15@gmail.com>
2026-03-11 12:32:49 +0800

    navigator layout

---
+++ app/src/main/java/com/example/navi/MainViewHome.kt
@@ -0,0 +1,44 @@
+package com.example.navi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.navi.ui.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainViewHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_view_home)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, HomeFragment())
+                .commit()
+        }
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        bottomNav.setOnItemSelectedListener { item ->
+
+            when(item.itemId){
+
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, HomeFragment())
+                        .commit()
+                }
+
+                R.id.nav_profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, ProfileFragment())
+                        .commit()
+                }
+
+            }
+
+            true
+        }
+    }
+}
--- app/src/main/java/com/example/navi/ui/MainView.kt
+++ app/src/main/java/com/example/navi/ui/MainView.kt
@@ -12,6 +12,8 @@ import android.widget.SearchView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat.enableEdgeToEdge
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,26 +23,24 @@ import com.example.navi.data.AppDatabase
 import com.example.navi.data.FilmDao
 import kotlinx.coroutines.launch
 
-class MainView : AppCompatActivity() {
+class HomeFragment : Fragment(R.layout.activity_main_view) {
     private lateinit var filmDao: FilmDao
     private lateinit var recyclerFilm: RecyclerView
     private lateinit var adapter: FilmAdapter
     @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main_view)
-        val db = AppDatabase.Companion.getDatabase(this)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val db = AppDatabase.getDatabase(requireContext())
         filmDao = db.filmDao()
 
         //promo iklan
-        val promoOverlay = findViewById<LinearLayout>(R.id.promoOverlay)
-        val imgPromoPoster = findViewById<ImageView>(R.id.imgPromoPoster)
-        val tvPromoTitle = findViewById<TextView>(R.id.tvPromoTitle)
-        val tvPromoGenre = findViewById<TextView>(R.id.tvGenre)
-        val btnPromoClose = findViewById<ImageView>(R.id.btnPromoClose)
-        val btnPromoBuy = findViewById<TextView>(R.id.btnPromoBuy)
-        val searchView = findViewById<SearchView>(R.id.searchView)
+        val promoOverlay = view.findViewById<LinearLayout>(R.id.promoOverlay)
+        val imgPromoPoster = view.findViewById<ImageView>(R.id.imgPromoPoster)
+        val tvPromoTitle = view.findViewById<TextView>(R.id.tvPromoTitle)
+        val tvPromoGenre = view.findViewById<TextView>(R.id.tvGenre)
+        val btnPromoClose = view.findViewById<ImageView>(R.id.btnPromoClose)
+        val btnPromoBuy = view.findViewById<TextView>(R.id.btnPromoBuy)
+        val searchView = view.findViewById<SearchView>(R.id.searchView)
 
 
 
@@ -69,13 +69,14 @@ class MainView : AppCompatActivity() {
                 tvPromoTitle.text = film.judul
                 tvPromoGenre.text = film.genre
 
-                Glide.with(this@MainView)
+                Glide.with(requireContext())
                     .load(film.posterUri)
                     .into(imgPromoPoster)
 
                 btnPromoBuy.setOnClickListener {
+                    val intent = Intent(requireContext(), DetailFilmActivity::class.java)
 
-                    val intent = Intent(this@MainView, DetailFilmActivity::class.java)
+
 
                     intent.putExtra("filmId", film.idFilm)
                     intent.putExtra("judul", film.judul)
@@ -103,52 +104,56 @@ class MainView : AppCompatActivity() {
 
 
 
-        recyclerFilm = findViewById(R.id.recyclerFilm)
-        recyclerFilm.layoutManager = GridLayoutManager(this, 2)
+        recyclerFilm = view.findViewById(R.id.recyclerFilm)
+        recyclerFilm.layoutManager = GridLayoutManager(requireContext(), 2)
 
         loadData()
 
-        val btnDetail = findViewById<ImageView>(R.id.btnMenu)
+        val btnDetail = view.findViewById<ImageView>(R.id.btnMenu)
 
         btnDetail.setOnClickListener {
 
             val bottomSheet = FilmBottomSheet()
-            bottomSheet.show(supportFragmentManager, "FilmBottomSheet")
+            bottomSheet.show(parentFragmentManager, "FilmBottomSheet")
 
         }
 
     }
 
 
-    override fun onResume() {
-        super.onResume()
-        loadData()   // 🔥 reload setiap kembali ke halaman ini
-    }
+        override fun onResume() {
+            super.onResume()
+            loadData()
+        }
 
-    private fun loadData() {
-        lifecycleScope.launch {
-            val listFilm = filmDao.getAllFilm()
-            adapter = FilmAdapter(
-                listFilm = listFilm,
-                onItemClick = { film ->
+        private fun loadData() {
 
-                    val intent = Intent(this@MainView, DetailFilmActivity::class.java)
-                    intent.putExtra("filmId", film.idFilm)
-                    intent.putExtra("judul", film.judul)
-                    intent.putExtra("genre", film.genre)
-                    intent.putExtra("durasi", film.durasi)
-                    intent.putExtra("director", film.director)
-                    intent.putExtra("synopsis", film.synopsis)
-                    intent.putExtra("tanggal", film.jadwaltayang)
-                    intent.putExtra("harga", film.hargaTiket)
-                    intent.putExtra("poster", film.posterUri)
-                    intent.putExtra("trailer", film.trailerId)
+            viewLifecycleOwner.lifecycleScope.launch {
 
-                    startActivity(intent)
-                }
-            )
+                val listFilm = filmDao.getAllFilm()
 
-            recyclerFilm.adapter = adapter
+                adapter = FilmAdapter(
+                    listFilm = listFilm,
+                    onItemClick = { film ->
+
+                        val intent = Intent(requireContext(), DetailFilmActivity::class.java)
+
+                        intent.putExtra("filmId", film.idFilm)
+                        intent.putExtra("judul", film.judul)
+                        intent.putExtra("genre", film.genre)
+                        intent.putExtra("durasi", film.durasi)
+                        intent.putExtra("director", film.director)
+                        intent.putExtra("synopsis", film.synopsis)
+                        intent.putExtra("tanggal", film.jadwaltayang)
+                        intent.putExtra("harga", film.hargaTiket)
+                        intent.putExtra("poster", film.posterUri)
+                        intent.putExtra("trailer", film.trailerId)
+
+                        startActivity(intent)
+                    }
+                )
+
+                recyclerFilm.adapter = adapter
+            }
         }
     }
-}
